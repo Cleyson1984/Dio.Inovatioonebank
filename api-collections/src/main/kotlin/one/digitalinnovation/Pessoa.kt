5aaee9652495612f9ed61643f1ea8697a7a40ef2
Cleyson <cleyson.romero@gmail.com>
2022-07-01 08:52:02 -0300

    update to apple the private modifiler set funcion at cpf property

--- api-collections/src/main/kotlin/one/digitalinnovation/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/Pessoa.kt
@@ -2,12 +2,15 @@ package one.digitalinnovation
 
 class Pessoa{
     var NOME: String = "Cleyson"
-    var CPF: String = "001.002.0003.00"
+
+    var CPF: String = "001.002.003.00"
+    private set
 }
 
 fun main() {
     val Cleyson = Pessoa()
 
+    println(Cleyson)
     println(Cleyson.NOME)
     println(Cleyson.CPF)
 }
