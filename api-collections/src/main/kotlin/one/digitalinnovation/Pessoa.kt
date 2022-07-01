fe25df9c29e281fce3e53b80a8477184fe2fb1b5
Cleyson <cleyson.romero@gmail.com>
2022-07-01 06:54:09 -0300

    created version class

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/Pessoa.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation
+
+class Pessoa{
+    var NOME: String = "Cleyson"
+    var CPF: String = "001.002.0003.00"
+}
+
+fun main() {
+    val Cleyson = Pessoa()
+
+    println(Cleyson.NOME)
+    println(Cleyson.CPF)
+}
