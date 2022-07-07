7c5ac5911e6b6daa1233648c3dfd6d67c38e9a6e
Cleyson <cleyson.romero@gmail.com>
2022-07-06 22:53:29 -0300

    created save class test to it (revisão)

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/teste/BancoTeste.kt
@@ -0,0 +1,10 @@
+package one.digitalinnovation.teste
+
+import one.digitalinnovation.Banco
+
+fun main() {
+    val digiOneBank = Banco("DigiOne"12)
+
+            println(digiOneBank.nome)
+            println(digiOneBank.numero)
+}
