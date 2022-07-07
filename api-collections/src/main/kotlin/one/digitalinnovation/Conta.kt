7a8ef9861ab582e67d775978c544a5dd05462466
Cleyson <cleyson.romero@gmail.com>
2022-07-06 21:47:44 -0300

    created account class

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/Conta.kt
@@ -0,0 +1,15 @@
+package one.digitalinnovation
+
+class Conta(
+    val agencia: String,
+    val numero: String,
+    val saldo: BigDecimal
+) {
+    fun deposito(valor: BigDecimal) {
+
+    }
+    fun saque(valor: BigDecimal) {
+
+    }
+
+}
