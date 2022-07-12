6e61f09bb2c591888e3ad9395ae97032e298455c
Cleyson <cleyson.romero@gmail.com>
2022-07-11 23:33:19 -0300

    updated to make person and employee abstract class

--- api-collections/src/main/kotlin/one/digitalinnovation/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/Pessoa.kt
@@ -1,18 +1,9 @@
 package one.digitalinnovation
 
-class Pessoa{
-    var NOME: String = "Cleyson"
+abstract class Pessoa(
+   val nome: String,
+   val cpf: String
 
-    var CPF: String = "001.002.003.00"
-    private set
+)
 
-    constructor()
 
-    fun pessoaInfo() = "$NOME = $CPF"
-
-}
-
-fun main() {
-    val Cleyson = Pessoa()
-    println(Cleyson.pessoaInfo())
-}
